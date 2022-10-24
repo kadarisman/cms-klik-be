c5660d590608aadb951ed52bdd5858e32a79cd48
kadarisman <yuzarqadariesman@gmail.com>
2022-10-24 22:34:20 +0700

    add auth middleware in config-blog router

--- src/routers/blogConfig.js
+++ src/routers/blogConfig.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const middleware = require ('../middleware/Auth');
 const { getBlogCOnfig, updateBlogConfig } = require('.././controllers/BlogConfigController');
 
-router.get('/', getBlogCOnfig);
+router.get('/', middleware.isAuth, getBlogCOnfig);
 router.put('/:id', middleware.isAuth, updateBlogConfig);
 
 module.exports = router;
